a265942ae08729255826b8f342efa4744b58593b
Mathieu17M <mathieu.mayard@epitech.eu>
2025-01-23 22:04:33 +0100

    [ADD] server handling

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 const app = express();
 dotenv.config();
 
 const PORT = process.env.BACKEND_PORT;
+const FRONTEND_PORT = process.env.FRONTEND_PORT;
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+app.use(cors( {
+    origin: `http://localhost:${FRONTEND_PORT}`,
+    credentials: true
+}));
