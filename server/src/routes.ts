710c84b076cd9e94030472f4b0ddda43d2ab896b
root <root@ordi-thomas.localdomain>
2025-01-24 09:45:42 +0100

    [ADD] add sign_in sign_up

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,29 @@
 import { Router } from 'express';
-import { handlerA, handlerB } from './api/apiHandler';
+import { body } from 'express-validator';
+import { sign_in, login } from './api/login';
 
 const router = Router();
 
-// Routes associées aux API
+// Route pour sign_in
+router.post(
+    '/sign_in',
+    [
+        body('firstName').notEmpty().withMessage('First name is required'),
+        body('lastName').notEmpty().withMessage('Last name is required'),
+        body('email').isEmail().withMessage('Invalid email address'),
+        body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+    ],
+    sign_in
+);
+
+// Route pour login
+router.post(
+    '/login',
+    [
+        body('email').isEmail().withMessage('Invalid email address'),
+        body('password').notEmpty().withMessage('Password is required'),
+    ],
+    login
+);
 
 export default router;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-
+import routes from './routes';
+import connectDB from './db/db';
 const app = express();
 dotenv.config();
 
 const PORT = process.env.BACKEND_PORT;
 const FRONTEND_PORT = process.env.FRONTEND_PORT;
+connectDB();
+
+app.use(express.json());
 
 app.use(cors( {
     origin: `http://localhost:${FRONTEND_PORT}`,
     credentials: true
 }));
+
+app.use('/api', routes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
