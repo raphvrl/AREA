06378a8d49051d68b019ecdc98a380b6b04ab037
Raphael Verrouil <raphael.verrouil@epitech.eu>
2025-01-31 14:58:48 +0100

    [ADD] more service

--- mobile/utils/areaMap.ts
+++ mobile/utils/areaMap.ts
@@ -1,6 +1,14 @@
 export const actionsMap = {
   repoCreated_github: "création d'un dépôt GitHub",
-  checkNewSong_spotify: "nouvelle chanson est ajoutée",
+  checkNewSong_spotify: "nouvelle chanson est ajoutée à Spotify",
+  playlistCreated_spotify: "création d'une playlist Spotify",
+  fileUploaded_dropbox: "fichier est ajouté à Dropbox",
+  folderCreated_dropbox: "création d'un dossier Dropbox",
+  fileDeleted_dropbox: "fichier est supprimé de Dropbox",
+  trackLiked_spotify: "chanson est aimée sur Spotify",
+  repoStarred_github: "dépôt est étoilé sur GitHub",
+  followerAdded_github: "nouveau follower GitHub",
+  pageCreated_notion: "création d'une page Notion",
 };
 
 export const reactionMap = {
