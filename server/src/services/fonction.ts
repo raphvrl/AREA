1512cf003eaed50d7b904321c269d36ed1854812
ThomasGaboriaud <thomas.gaboriaud@epitech.eu>
2025-02-03 01:43:38 +0100

    [ADD] loginOauthSpotify

--- server/src/services/fonction.ts
+++ server/src/services/fonction.ts
@@ -1,11 +1,20 @@
-export const time10Seconde = async (): Promise<boolean> => {
+export const time10Seconde = async (
+  email: string,
+  option?: string
+): Promise<boolean> => {
   return new Promise(resolve => {
+    console.log(option);
     setTimeout(() => {
       resolve(true);
     }, 5000); // Timer de 5 secondes
   });
 };
 
-export const sendmessageTerminal = async (): Promise<void> => {
+export const sendmessageTerminal = async (
+  email: string,
+  result?: string,
+  option?: string
+): Promise<void> => {
+  console.log(option);
   console.log('Hello, comment va tu ?');
 };
